f0aec21c72c6c8d44d02132272706750695e9373
Alesya Hitrova <alesyahitrova@yandex.ru>
2023-05-16 20:24:52 +0300

    hw12

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,14 +1,13 @@
 package ru.netology.nmedia.viewmodel
 
 import android.app.Application
-import android.view.View
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
-import ru.netology.nmedia.repository.PostRepositoryInMemory
+import ru.netology.nmedia.repository.PostRepositorySQLiteImpl
 
 private val empty = Post(
     id = 0,
@@ -20,15 +19,17 @@ private val empty = Post(
 
 
 class PostViewModel(application: Application) : AndroidViewModel(application) {
-    private val repository: PostRepository = PostRepositoryInMemory(application)
-    val data: LiveData<List<Post>> = repository.getData()
+    private val repository: PostRepository = PostRepositorySQLiteImpl(
+        AppDb.getInstance(application).postDao
+    )
+    val data: LiveData<List<Post>> = repository.getAll()
+    val edited = MutableLiveData(empty)
+
     fun likeById(id: Long) = repository.likeById(id)
     fun shareById(id: Long) = repository.shareById(id)
     fun removeById(id: Long) = repository.removeById(id)
 
     fun viewPostById(id: Long) = repository.viewPostById(id)
-
-    val edited = MutableLiveData(empty)
 
     fun save() {
         edited.value?.let {
@@ -52,6 +53,5 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
             }
         }
     }
-
 
 }
